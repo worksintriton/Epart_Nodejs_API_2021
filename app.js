1457ed19884a931049cfce3bd785c881176649a9
sivapriyan20 <worksintriton@gmail.com>
2021-04-21 15:31:29 +0530

    Code Merge

--- app.js
+++ app.js
@@ -12,12 +12,20 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const auth = require('./routes/auth');
-const buyerManagement = require('./routes/buyerManagement');
+const buyerManagement = require('./routes/buyerMangement');
 const buyerDetails = require('./routes/buyerDetails');
 
 app.use('/auth', auth);
 app.use('/buyer_management', buyerManagement);
 app.use('/buyer_details', buyerDetails);
+
+
+server.listen(port,function(err){
+    if(err) 
+    console.log("Server Startup Error: "+err);
+    else
+    console.log("Server running @port: "+port);
+});
 
 app.get('/',function(req,res){
     res.writeHead(200,{'Content-Type':'text/html'});
